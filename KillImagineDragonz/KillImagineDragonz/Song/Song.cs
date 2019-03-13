f5b8cf0f353cc3d5f66e861e3c2c63d2e6a0928c
Maggie Leavell <mjleavell@gmail.com>
2019-03-12 21:03:21 -0500

    created song class

---
+++ KillImagineDragonz/KillImagineDragonz/Song/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KillImagineDragonz.Song
+{
+    class Song
+    {
+        // Define a class called `Song` that has a string property `Artist` and a string property `Name`. 
+        // Both properties should be required(invariant) in order to construct an instance of `Song`
+
+        public string Artist { get; }
+        public string Name { get; }
+
+        public Song(string artistName, string songName)
+        {
+            Artist = artistName;
+            Name = songName;
+        }
+
+    }
+}
